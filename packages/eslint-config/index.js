50fe320df7c2bfe86b5411f421cdfe82fa3c926f
Fery Wardiyanto <ferywardiyanto@gmail.com>
2021-07-13 12:06:07 +0700

    feat: overwrite prettier printWidth config
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,16 +1,15 @@
 module.exports = {
   extends: ['standard', 'prettier'],
   plugins: ['prettier'],
-  ignorePatterns: [
-    'dist/**',
-    'node_modules/',
-    'tests/**',
-    '**.old',
-    '**.cjs',
-    '**.mjs'
-  ],
+  ignorePatterns: ['dist/**', 'node_modules/', 'tests/**', '**.old', '**.cjs', '**.mjs'],
   rules: {
-    'prettier/prettier': ['error'],
+    'prettier/prettier': [
+      'error',
+      {
+        ...require('prettier-config-standard'),
+        printWidth: 120
+      }
+    ],
     'no-unused-vars': 0,
     'one-var': 0
   }
