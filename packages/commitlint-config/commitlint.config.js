1863d7af57214ce0216c6e3e0add209443a3f36e
Fery Wardiyanto <ferywardiyanto@gmail.com>
2021-07-21 17:38:56 +0700

    feat(commitlint): add type-enum rule
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- packages/commitlint-config/commitlint.config.js
+++ packages/commitlint-config/commitlint.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [2, 'always', Infinity]
+    'body-max-line-length': [2, 'always', Infinity],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
+    ]
   }
 }
